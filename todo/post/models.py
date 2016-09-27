1270efecf3582a7ffee4cd42d6e6c970382cd1c7
Changhyun Lee <chang12@snu.ac.kr>
2016-09-27 22:20:04 +0900

    Task 모델 / TaskForm 모델폼 / Index 뷰 뼈대

--- todo/post/models.py
+++ todo/post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# priority를 float 타입으로 설정하는 이유:
+# 새로 등록할 경우 -> 기존 맥시멈 + a
+# 우선순위를 위아래로 변경할 경우 -> 끼어들어가야 할 자리 위/아래 priority 값의 평균
+# 이 방식을 따를 경우 어떤 task의 우선순위를 변경해도, 다른 taks들의 우선순위 값 변동이 없다!
+class Task(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    is_active = models.BooleanField(default=True)
+    priority = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- todo/post/views.py
+++ todo/post/views.py
@@ -1,8 +1,12 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 
+from .forms import TaskForm
+
 
 @staff_member_required()
 def index(request):
-    return render(request, "post/index.html")
+    return render(request, "post/index.html", {
+        'form': TaskForm,
+    })
 

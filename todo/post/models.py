78ebdc757876f4b05aa9130879fcfee5f85ece1f
Changhyun Lee <chang12@snu.ac.kr>
2016-10-04 23:42:54 +0900

    상승/하강 버튼에 부여할 id 반환하는 model method, 적용

--- todo/post/models.py
+++ todo/post/models.py
@@ -6,6 +6,9 @@ from django.db import models
 # 우선순위를 위아래로 변경할 경우 -> 끼어들어가야 할 자리 위/아래 priority 값의 평균
 # 이 방식을 따를 경우 어떤 task의 우선순위를 변경해도, 다른 taks들의 우선순위 값 변동이 없다!
 class Task(models.Model):
+    BUTTON_UP_TAG = "up"
+    BUTTON_DOWN_TAG = "down"
+
     title = models.CharField(max_length=30)
     content = models.TextField(blank=True)
     is_active = models.BooleanField(default=True)
@@ -13,12 +16,18 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
+    def button_id_up(self):
+        return Task.BUTTON_UP_TAG + '-' + str(self.pk)
+
+    def button_id_down(self):
+        return Task.BUTTON_DOWN_TAG + '-' + str(self.pk)
+
     def save(self, *args, **kwargs):
         if Task.objects.count() == 0:
             self.priority = 0
         else:
             self.priority = Task.objects.all().aggregate(models.Max('priority'))['priority__max'] + 1.0
         super(Task, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.title
